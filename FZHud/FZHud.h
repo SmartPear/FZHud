ee80b9e964b10b09fdd1ed53e733564cb31a61dc
王欣 <wangxinobserver@gmail.com>
2020-04-12 14:31:26 +0800

    第一次提交

---
+++ FZHud/FZHud.h
@@ -0,0 +1,19 @@
+//
+//  FZHud.h
+//  FZHud
+//
+//  Created by 王欣 on 4/12/20.
+//  Copyright © 2020 QFPay. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FZHud.
+FOUNDATION_EXPORT double FZHudVersionNumber;
+
+//! Project version string for FZHud.
+FOUNDATION_EXPORT const unsigned char FZHudVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FZHud/PublicHeader.h>
+
+
---
+++ Podfile
@@ -0,0 +1,19 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'FZHud' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+  pod 'NVActivityIndicatorView'
+
+  # Pods for FZHud
+
+end
+
+target 'HudDemo' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for HudDemo
+
+end
