2dcf94431befbbbbfcc742e98927238c2324f7b2
王欣 <wangxinobserver@gmail.com>
2020-04-12 14:31:26 +0800

    第一次提交

---
+++ HudDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HudDemo
+//
+//  Created by 王欣 on 4/12/20.
+//  Copyright © 2020 QFPay. All rights reserved.
+//
+
+import UIKit
+import FZHud
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+
+
+}
+
